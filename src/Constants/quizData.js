266c40023f2529b513b39f9c306ed60fce00e673
Abhi9125 <abhi.singh3132@gmail.com>
2024-11-01 15:30:17 +0530

    Add data

---
+++ src/Constants/quizData.js
@@ -0,0 +1,81 @@
+const quizQuestions = [
+  {
+    question: "What is the output of '2' + 2 in JavaScript?",
+    choices: ["22", "4", "Error", "NaN"],
+    correctAnswer: "22",
+  },
+  {
+    question: "Which of the following is not a JavaScript data type?",
+    choices: ["Undefined", "Number", "Float", "String"],
+    correctAnswer: "Float",
+  },
+  {
+    question: "How do you declare a JavaScript variable?",
+    choices: [
+      "variable carName;",
+      "var carName;",
+      "v carName;",
+      "dim carName;",
+    ],
+    correctAnswer: "var carName;",
+  },
+  {
+    question: "Which operator is used to assign a value to a variable?",
+    choices: ["-", "=", "*", "x"],
+    correctAnswer: "=",
+  },
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<js>", "<javascript>", "<scripting>", "<script>"],
+    correctAnswer: "<script>",
+  },
+  {
+    question: "How do you write 'Hello World' in an alert box?",
+    choices: [
+      "msg('Hello World');",
+      "alertBox('Hello World');",
+      "msgBox('Hello World');",
+      "alert('Hello World');",
+    ],
+    correctAnswer: "alert('Hello World');",
+  },
+  {
+    question:
+      "Which function is used to serialize an object into a JSON string in JavaScript?",
+    choices: ["stringify()", "parse()", "convert()", "None of the above"],
+    correctAnswer: "stringify()",
+  },
+  {
+    question: "How do you create a function in JavaScript?",
+    choices: [
+      "function myFunction()",
+      "function:myFunction()",
+      "function = myFunction()",
+      "None of the above",
+    ],
+    correctAnswer: "function myFunction()",
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choices: [
+      "<script name='xxx.js'>",
+      "<script src='xxx.js'>",
+      "<script href='xxx.js'>",
+      "<script link='xxx.js'>",
+    ],
+    correctAnswer: "<script src='xxx.js'>",
+  },
+  {
+    question: "How can you add a comment in JavaScript?",
+    choices: [
+      "<!--This is a comment-->",
+      "//This is a comment",
+      "'This is a comment",
+      "/*This is a comment*/",
+    ],
+    correctAnswer: "//This is a comment",
+  },
+];
+
+export default quizQuestions;
